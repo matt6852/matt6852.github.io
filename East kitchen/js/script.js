0eff4d5ca6a33565430cb45f1d5aa93e710eafe6
matt6852 <72603646+matt6852@users.noreply.github.com>
2020-12-21 10:47:13 +0300

    Update script.js

--- East kitchen/js/script.js
+++ East kitchen/js/script.js
@@ -1,78 +1,55 @@
-$(document).ready(function (){
+$(document).ready(function () {
   // toggler btn
-  $(".navbar-toggler").click(function(){
-
-    $(".navbar-toggler").toggleClass("change")
-  })
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("change");
+  });
   //sticky nav
-  $(window).scroll(function(){
-    let position =$(this).scrollTop()
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
 
-    if(position>=799){
-      $(".navbar").addClass("navbar-background")
-      $(".navbar").addClass("fixed-top")
-    }
-    else{
+    if (position >= 699) {
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    } else {
       $(".navbar").removeClass("navbar-background");
       $(".navbar").removeClass("fixed-top");
     }
-  })
-
+  });
 
   // smooth scrol
-  $(".nav-item a, .header-link, #back-to-top ").click(function(link){
+  $(".nav-item a, .header-link, #back-to-top ").click(function (link) {
     link.preventDefault();
-
 
     let target = $(this).attr("href");
 
-    $("html, body").stop().animate({
-      scrollTop: $(target).offset().top - 25
-    }, 1000);
-
-  })
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top - 25,
+        },
+        1000
+      );
+  });
 
   // back to-top
 
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
 
-   $(window).scroll(function () {
-     let position = $(this).scrollTop();
+    if (position >= 799) {
+      $("#back-to-top").addClass("scrollTop");
+    } else {
+      $("#back-to-top").removeClass("scrollTop");
+    }
+  });
 
-     if (position >= 799) {
-       $("#back-to-top").addClass("scrollTop");
-    
-     } else {
-        $("#back-to-top").removeClass("scrollTop");
-   
-     }
-   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    $('.parent-container').magnificPopup({
-  delegate: 'a', // child items selector, by clicking on it popup will open
-  type: 'image',
-  gallery:{
-    enabled:true
-  }
-  // other options
+  $(".parent-container").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+    // other options
+  });
 });
-})
