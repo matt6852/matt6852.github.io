b720def1746e71327a2d5c866c486614a508cc11
matt6852 <72603646+matt6852@users.noreply.github.com>
2020-12-29 12:04:09 +0300

    scrolling navbar

--- Get flex/app.js
+++ Get flex/app.js
@@ -1,7 +1,27 @@
-document.querySelector(".nav__btn").addEventListener("click",showNav)
+$(document).ready(function () {
+  document.querySelector(".nav__btn").addEventListener("click", showNav);
+
+  function showNav() {
+    document.querySelector(".nav__links").classList.toggle("show__links");
+  }
 
 
+  
+  // smooth scrol
+  $(".nav__single-link").click(function (link) {
+    link.preventDefault();
 
-function showNav(){
-    document.querySelector(".nav__links").classList.toggle("show__links")
-}
+    let target = $(this).attr("href");
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top - 25,
+        },
+        1000
+      );
+  });
+
+  // back to-top
+});
