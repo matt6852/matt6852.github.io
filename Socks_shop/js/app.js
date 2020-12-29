870677075f60c31fc3a2c27940e9ec0dc75b2462
matt6852 <72603646+matt6852@users.noreply.github.com>
2020-12-29 12:50:58 +0300

    scrolled navbar

--- Socks_shop/js/app.js
+++ Socks_shop/js/app.js
@@ -1,6 +1,22 @@
-document.getElementById("cart-info").addEventListener("click", function(){
-    const cart = document.getElementById("cart")
+$(document).ready(function () {
+  document.getElementById("cart-info").addEventListener("click", function () {
+    const cart = document.getElementById("cart");
     cart.classList.toggle("show-cart");
     console.log(cart);
-    
-})
+  });
+
+  $(".nav-item a").click(function (link) {
+    link.preventDefault();
+
+    let target = $(this).attr("href");
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top - 25,
+        },
+        1000
+      );
+  });
+});
